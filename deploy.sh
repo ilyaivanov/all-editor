4a507ffd0d544b4dbc671fd3b0919c6dd74b07dc
Ilya Ivanov <static.ila@gmail.com>
2024-12-21 01:56:30 +0200

    Fix PWA issue and deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,10 @@
 
 echo "Removing all files from ../slapstuk/build"
 rm -r ../slapstuk/build
-npm run build
 echo "Copying files to ../slapstuk folder..."
 cp -r ./build  ../slapstuk
 echo "Running 'firebase deploy --only hosting' from ../slapstuk folder..."
 pushd ../slapstuk
-firebase deploy --only hosting -P tubeflowy
+firebase deploy --only hosting -P ilyatest-515cf 
 popd
 echo "Deploy to Slapstuk hosting done."
